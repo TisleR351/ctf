2013c3ba89bc146375ccb306c50de8f3b4689c87
tisler <mathis.ranson@icloud.com>
2024-12-05 11:51:41 +0100

    [FEAT] Background

---
+++ app/components/background/background.tsx
@@ -0,0 +1,11 @@
+import "./background.css";
+
+export default function Background() {
+  return (
+    <div className="background">
+      <div className="text-line medium">CAPTURE</div>
+      <div className="text-line small">THE FLAG</div>
+      <div className="text-line large">ESIGELEC</div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Overlay from "@/app/components/overlay/overlay";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Background from "@/app/components/background/background";
 
 export default function Home() {
   return (
     <div>
+      <Background />
       <Overlay />
     </div>
   );
